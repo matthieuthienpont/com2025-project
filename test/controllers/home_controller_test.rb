9e755bec842a24d45614a3d853df06218fb14581
mt00759 <mt00759@surrey.ac.uk>
2018-12-04 14:15:42 +0000

    Added contact mailer

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -30,7 +30,7 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should post request contact" do
     post '/request_contact',
       params: {name: "Matthew", email: "matthew@me.com",
-      telephone: "1234567890", message: "Hello"}
+        telephone: "1234567890", message: "Hello"}
     assert_response :redirect
     assert_nil flash[:alert]
     assert_not_empty flash[:notice]
--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ContactMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return contact email" do
+    mail = ContactMailer.contact_email("matthew@me.com",
+      "Matthew Casey", "1234567890", @message = "Hello")
+      assert_equal ['to@example.com'], mail.to
+      assert_equal ['from@example.com'], mail.from
+end
 end
