bdbce0a807e2b8b2c183a5a885b60be0481a4b39
mt00759 <mt00759@surrey.ac.uk>
2018-12-07 21:59:04 +0000

    Album scaffold v3

---
+++ db/migrate/20181207215847_create_albums.rb
@@ -0,0 +1,14 @@
+class CreateAlbums < ActiveRecord::Migration[5.2]
+  def change
+    create_table :albums do |t|
+      t.string :title
+      t.string :artist
+      t.string :composer
+      t.string :genre
+      t.date :year
+      t.string :comments
+
+      t.timestamps
+    end
+  end
+end
